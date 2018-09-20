3887fb6f8c81f2e11b92bd88633fb5e4f8b51537
Benoit GENEVAUX <benoit.genevaux@kleegroup.com>
2018-09-20 22:01:39 +0200

    Starting reporting

--- src/fr/kata/Reporting.java
+++ src/fr/kata/Reporting.java
@@ -1,9 +1,36 @@
 package fr.kata;
 
+import java.util.EnumMap;
+import java.util.Map;
+
+import fr.kata.CustomerOrder.Drink;
+
 public class Reporting {
 	
+	public static class DrinkStat {
+		private int nbServed;
+	}
+	
+	private static final Map<Drink, DrinkStat> stats = new EnumMap<>(Drink.class);
+	
+	private static String buildDrinkStat(final Drink drink) {
+		final DrinkStat stat;
+		if (stats.containsKey(drink)) {
+			stat = stats.get(drink);
+		} else {
+			stat = new DrinkStat();
+		}
+		final StringBuilder sb = new StringBuilder();
+		sb.append(drink.name())
+			.append(';')
+			.append(stat.nbServed);
+		return sb.toString();
+	}
+	
 	public static void showConsole() {
-		
+		for (final Drink drink : CustomerOrder.Drink.values()) {
+			System.out.println(buildDrinkStat(drink));
+		}
 	}
 	
 	public static void toCsvFile(final String filePath) {
